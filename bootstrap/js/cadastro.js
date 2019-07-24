55256edf2eaf2a8a3ee50880f8f4869be80d3d4d
NAZIANO FELIX DE ALMEIDA JUNIOR <a9187448@rn.senac.br>
2019-07-23 21:24:54 -0300

    primeiro commit

---
+++ bootstrap/js/cadastro.js
@@ -0,0 +1,95 @@
+
+function cadastrar() {
+
+   
+    // let nome = document.getElementById('nome').value;
+    let nome = validaNome(document.getElementById('nome').value);
+    if (nome == false) {
+        alert('Insira um nome válido!');
+        document.getElementById('nome').focus();
+        return;
+    }
+
+    
+    let fone = document.getElementById('fone').value;
+    
+
+    let cidade = document.getElementById('cidade').value;
+    
+
+    let sexo = '';
+    if (document.getElementById('masc').checked) {
+        sexo = 'Masculino';
+    } else if (document.getElementById('feme').checked) {
+        sexo = 'Feminino';
+    } else {
+        sexo = 'Prefiro não comentar';
+    }
+   
+
+    insereNaTabela(nome, fone, sexo, cidade);
+
+    limparFormulario();
+
+}
+
+function limparFormulario() {
+
+    document.getElementById('nome').value = '';
+    document.getElementById('fone').value = '';
+    document.getElementById('cidade').value = 'Selecione';
+    document.getElementById('masc').checked = true;
+    document.getElementById('nome').focus();
+
+}
+
+
+function validaNome(nome) {
+
+    let texto = nome.trim().toUpperCase();
+    for (let i = 0; i < texto.length; i++) {
+        if (ehNumero(texto[i])) {
+            return false;
+        }
+    }
+    return texto;
+}
+
+function ehNumero(numero) {
+    return !isNaN(numero);
+}
+
+function insereNaTabela(nome, fone, sexo, cidade) {
+    let tabela = document.getElementById('lista-contatos')
+                .getElementsByTagName('tbody')[0];
+
+    let ultimaLinha = tabela.rows.length;
+
+    let linha = tabela.insertRow(ultimaLinha);
+
+    let campoId = linha.insertCell(0);
+    let campoNome = linha.insertCell(1);
+    let campoFone = linha.insertCell(2);
+    let campoSexo = linha.insertCell(3);
+    let campoCidade = linha.insertCell(4);
+    let editar = linha.insertCell(5);
+
+    campoId.innerHTML = ultimaLinha + 1;
+    campoNome.innerHTML = nome;
+    campoFone.innerHTML = fone;
+    campoSexo.innerHTML = sexo;
+    campoCidade.innerHTML = cidade;
+    editar.innerHTML = insereBotoesAcoes();
+}
+
+function insereBotoesAcoes() {
+    let botaoEditar = '<button type="button" class="btn btn-info btn-sm">';
+    botaoEditar += '<i class="fas fa-user-edit"></i>';
+    botaoEditar += '</button>';
+
+    let botaoRemover = '<button type="button" class="btn btn-danger btn-sm">';
+    botaoRemover += '<i class="fas fa-trash-alt"></i>';
+    botaoRemover += ' </button>';
+
+    return botaoEditar + botaoRemover;
+}
